3b44dd0d6f229edf1925b10650502206aff0bd74
TyMcMan <tyclipscomb@gmail.com>
2026-04-24 14:06:15 -0400

    changes

--- bigintadd.s
+++ bigintadd.s
@@ -1,9 +1,3 @@
-/*--------------------------------------------------------------------*/
-/* bigintadd.s                                                        */
-/* ARM64: BigInt_larger and BigInt_add (Part 2d: params/locals on     */
-/* stack; .equ for constants and offsets).                            */
-/*--------------------------------------------------------------------*/
-
 /* In lieu of a boolean data type. */
 
 /* enum {FALSE, TRUE} */
@@ -11,179 +5,180 @@
 .equ TRUE, 1
 .equ MAX_DIGITS, 32768
 
-/* struct BigInt field offsets (bytes) */
-.equ LLENGTH, 0
-.equ AAULDIGITS, 8
-
-/* BigInt_larger stack frame */
-.equ L_FRAME, 32
-.equ L_LENGTH1, 0
-.equ L_LENGTH2, 8
-.equ L_LLARGER, 16
-
-/* BigInt_add frame (all C locals + saved x30) */
-.equ ADD_FRAME, 64
-.equ ADD_X30, 0
-.equ O_ADDEND1, 8
-.equ O_ADDEND2, 16
-.equ O_SUM, 24
-.equ ULCARRY, 32
-.equ ULSUM, 40
-.equ LINDEX, 48
-.equ LSUMLENGTH, 56
 
 /*--------------------------------------------------------------------*/
+
 /* Return the larger of lLength1 and lLength2. */
-
-.text
 BigInt_larger:
-    sub sp, sp, L_FRAME
+   sub sp, sp, 32
+   
+   /* if (lLength1 > lLength2) */
+   cmp x0, x1
+   ble else1
 
-    str x0, [sp, L_LENGTH1]
-    str x1, [sp, L_LENGTH2]
+   /* lLarger = lLength1 */
+   str x0, [sp, 24]
+   b endif1
 
-    /* if (lLength1 > lLength2) */
-    ldr x0, [sp, L_LENGTH1]
-    ldr x1, [sp, L_LENGTH2]
-    cmp x0, x1
-    ble larger_else
+else1:
+   /* lLarger = lLength2 */
+   str x1, [sp, 24]
 
-    /* lLarger = lLength1 */
-    str x0, [sp, L_LLARGER]
-    b larger_endif
-
-larger_else:
-    /* lLarger = lLength2 */
-    str x1, [sp, L_LLARGER]
-
-larger_endif:
-    ldr x0, [sp, L_LLARGER]
-    add sp, sp, L_FRAME
-    ret
+endif1:
+   ldr x0, [sp, 24] 
+   add sp, sp, 32
+   ret
 
 /*--------------------------------------------------------------------*/
+
 /* Assign the sum of oAddend1 and oAddend2 to oSum.  oSum should be
    distinct from oAddend1 and oAddend2.  Return 0 (FALSE) if an
-   overflow occurred, and 1 (TRUE) otherwise. */ 
-   
+   overflow occurred, and 1 (TRUE) otherwise. 
+
+; int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
+; {
+;    unsigned long ulCarry;
+;    unsigned long ulSum;
+;    long lIndex;
+;    long lSumLength;
+
+;    /* Determine the larger length. */
+;    lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
+
+;    /* Clear oSum's array if necessary. */
+;    if (oSum->lLength > lSumLength)
+;       memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+
+   ; /* Perform the addition. */
+   ; ulCarry = 0;
+   ; for (lIndex = 0; lIndex < lSumLength; lIndex++)
+   ; {
+   ;    ulSum = ulCarry;
+   ;    ulCarry = 0;
+
+   ;    ulSum += oAddend1->aulDigits[lIndex];
+   ;    if (ulSum < oAddend1->aulDigits[lIndex]) /* Check for overflow. */
+   ;       ulCarry = 1;
+
+   ;    ulSum += oAddend2->aulDigits[lIndex];
+   ;    if (ulSum < oAddend2->aulDigits[lIndex]) /* Check for overflow. */
+   ;       ulCarry = 1;
+
+   ;    oSum->aulDigits[lIndex] = ulSum;
+   ; }
+*/ 
+
 .global BigInt_add
 BigInt_add:
-    sub sp, sp, ADD_FRAME
-    str x30, [sp, ADD_X30]
+   sub sp, sp, 64
+   str x30, [sp] 
 
-    str x0, [sp, O_ADDEND1]
-    str x1, [sp, O_ADDEND2]
-    str x2, [sp, O_SUM]
+   str x0, [sp, 16] /* store oAddend1 */
+   str x1, [sp, 24] /* store oAddend2 */
+   str x2, [sp, 32] /* store oSum */
 
-    /* lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength) */
-    ldr x0, [sp, O_ADDEND1]
-    ldr x0, [x0, LLENGTH]
-    ldr x1, [sp, O_ADDEND2]
-    ldr x1, [x1, LLENGTH]
-    bl BigInt_larger
-    str x0, [sp, LSUMLENGTH]
+   ldr x0, [x0] /* loads oAddend1->lLength since x is 8 byte and lLength is first 8 byte of struct*/
+   ldr x1, [x1] /* oAddend2->lLength */
 
-    /* if (oSum->lLength > lSumLength) memset(...) */
-    ldr x0, [sp, O_SUM]
-    ldr x4, [x0, LLENGTH]
-    ldr x3, [sp, LSUMLENGTH]
-    cmp x4, x3
-    ble add_skip_memset
+   bl BigInt_larger
 
-    ldr x0, [sp, O_SUM]
-    add x0, x0, AAULDIGITS
-    mov x1, 0
-    mov x2, #(MAX_DIGITS * 8)
-    bl memset
+   mov x3, x0 /* lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength) */
 
-add_skip_memset:
-    /* ulCarry = 0; lIndex = 0; */
-    mov x8, 0
-    str x8, [sp, ULCARRY]
-    str x8, [sp, LINDEX]
+   str x3, [sp, 40]
 
-add_loop:
-    ldr x6, [sp, LINDEX]
-    ldr x3, [sp, LSUMLENGTH]
-    cmp x6, x3
-    bge add_endloop
+   ldr x0, [sp, 32]
+   ldr x4, [x0] /* oSum->lLength */
 
-    /* ulSum = ulCarry; ulCarry = 0 */
-    ldr x7, [sp, ULCARRY]
-    str x7, [sp, ULSUM]
-    mov x8, 0
-    str x8, [sp, ULCARRY]
+   cmp x4, x3
+   ble endif1 /* if (oSum->lLength > lSumLength) */
 
-    /* ulSum += oAddend1->aulDigits[lIndex] */
-    ldr x0, [sp, O_ADDEND1]
-    add x0, x0, AAULDIGITS
-    ldr x9, [sp, LINDEX]
-    ldr x10, [x0, x9, lsl 3]
-    ldr x7, [sp, ULSUM]
-    add x7, x7, x10
-    str x7, [sp, ULSUM]
-    cmp x7, x10
-    bhs add_after_carry1
-    mov x8, 1
-    str x8, [sp, ULCARRY]
-add_after_carry1:
+   add x0, x0, 8 /* address of oSum->aulDigits (very start of array) */
+   mov x1, 0
+   mov x2, #(MAX_DIGITS * 8)
 
-    /* ulSum += oAddend2->aulDigits[lIndex] */
-    ldr x0, [sp, O_ADDEND2]
-    add x0, x0, AAULDIGITS
-    ldr x9, [sp, LINDEX]
-    ldr x10, [x0, x9, lsl 3]
-    ldr x7, [sp, ULSUM]
-    add x7, x7, x10
-    str x7, [sp, ULSUM]
-    cmp x7, x10
-    bhs add_after_carry2
-    mov x8, 1
-    str x8, [sp, ULCARRY]
-add_after_carry2:
+   bl memset
 
-    /* oSum->aulDigits[lIndex] = ulSum */
-    ldr x0, [sp, O_SUM]
-    add x0, x0, AAULDIGITS
-    ldr x9, [sp, LINDEX]
-    ldr x7, [sp, ULSUM]
-    str x7, [x0, x9, lsl 3]
+endif1:
+   ldr x3, [sp, 40]
+   mov x5, 0 /* ulCarry */
+   mov x6, 0 /* lIndex */
 
-    ldr x9, [sp, LINDEX]
-    add x9, x9, 1
-    str x9, [sp, LINDEX]
-    b add_loop
+loop1:
+   cmp x6, x3
+   bge endloop1
 
-add_endloop:
-    ldr x5, [sp, ULCARRY]
-    cmp x5, 1
-    bne add_write_length
+   mov x7, x5 /* ulSum */
+   mov x5, 0 /* ulCarry */
 
-    /* lSumLength == MAX_DIGITS (32768 == 8 << 12) */
-    ldr x3, [sp, LSUMLENGTH]
-    cmp x3, #8, lsl #12
-    bne add_carry_digit
+   ldr x0, [sp, 16]
+   add x0, x0, 8
+   ldr x0, [x0, x6, lsl 3] /* oAddend1->aulDigits[lIndex] */
+   add x7, x7, x0 /* ulSum += oAddend1->aulDigits[lIndex] */
 
-    mov w0, FALSE
-    ldr x30, [sp, ADD_X30]
-    add sp, sp, ADD_FRAME
-    ret
+   cmp x7, x0
+   bhs endif2
+   mov x5, 1
 
-add_carry_digit:
-    ldr x0, [sp, O_SUM]
-    add x0, x0, AAULDIGITS
-    ldr x3, [sp, LSUMLENGTH]
-    mov x11, 1
-    str x11, [x0, x3, lsl 3]
-    add x3, x3, 1
-    str x3, [sp, LSUMLENGTH]
+endif2:
+   ldr x1, [sp, 24]
+   add x1, x1, 8
+   ldr x1, [x1, x6, lsl 3] /* oAddend2->aulDigits[lIndex] */
+   add x7, x7, x1 /* ulSum += oAddend2->aulDigits[lIndex] */
 
-add_write_length:
-    ldr x0, [sp, O_SUM]
-    ldr x3, [sp, LSUMLENGTH]
-    str x3, [x0, LLENGTH]
+   cmp x7, x1
+   bhs endif3
+   mov x5, 1
 
-    mov w0, TRUE
-    ldr x30, [sp, ADD_X30]
-    add sp, sp, ADD_FRAME
-    ret
+endif3:
+   ldr x2, [sp, 32]
+   add x2, x2, 8
+   str x7, [x2, x6, lsl 3]
+
+   add x6, x6, 1
+
+   b loop1
+
+endloop1:
+
+   cmp x5, 1
+   bne endif4
+
+   cmp x3, MAX_DIGITS
+   bne endif5
+
+   move x0, FALSE
+   b finish
+
+endif5:
+   ldr x2, [sp, 32]
+   add x2, x2, 8
+   
+   mov x4, 1
+   str x4, [x2, x6, lsl 3]
+
+   add x3, x3, 1
+endif4:
+
+   ldr x2, [sp, 32]
+   str x3, [x2]
+
+   mov x0, TRUE
+   
+   /* Check for a carry out of the last "column" of the addition. */
+   if (ulCarry == 1)
+   {
+      if (lSumLength == MAX_DIGITS)
+         return FALSE;
+      oSum->aulDigits[lSumLength] = 1;
+      lSumLength++;
+   }
+
+   /* Set the length of the sum. */
+   oSum->lLength = lSumLength;
+
+   return TRUE;
+
+finish:
+   ldr x30, [sp]
+   add sp, sp, 64
+   ret
